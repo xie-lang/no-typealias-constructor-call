c753e1f863f23e5eba5a4f0830fdaf31f37dac34
LXie-Saintex <admin@saintex.co.uk>
2020-05-28 09:58:28 +0100

    modified comment format

--- src/NoAppExprForTypeAlias.elm
+++ src/NoAppExprForTypeAlias.elm
@@ -1,20 +1,20 @@
-module NoAppExprForTypeAlias exposing (..)
+module NoAppExprForTypeAlias exposing (rule)
 
 
-{- `NoAppExprForTypeAlias` forces you to use Record Expression for any type aliases declared in the current module -}
+{-| `NoAppExprForTypeAlias` forces you to use Record Expression for any type aliases declared in the current module -}
 
 import Elm.Syntax.Expression as Expression exposing (Expression)
 import Elm.Syntax.Declaration as Declaration exposing (Declaration)
 import Elm.Syntax.Node as Node exposing (Node)
 import Review.Rule as Rule exposing (Error, Rule, error, Direction)
 
--- Use context to store the names of all type aliases in the module
+{-| Use context to store the names of all type aliases in the module -}
 type alias Context = List String
 
 rule : Rule
 rule =
     Rule.newModuleRuleSchema "NoAppExprForTypeAlias" []
-    -- visit all declarations and expressions in the current module
+    {-| visit all declarations and expressions in the current module -}
         |> Rule.withDeclarationVisitor declarationVisitor
         |> Rule.withExpressionVisitor expressionVisitor
         |> Rule.fromModuleRuleSchema
@@ -23,12 +23,12 @@ rule =
 declarationVisitor : Node Declaration -> Direction -> Context -> ( List (Error {}), Context)
 declarationVisitor node direction context = 
     case Node.value node of 
-    {- check if the declaration has the type of [Type Alias]
+    {-| check if the declaration has the type of [Type Alias]
     (https://package.elm-lang.org/packages/stil4m/elm-syntax/latest/Elm-Syntax-TypeAlias)
     -}
         Declaration.AliasDeclaration n-> 
             case direction of 
-            -- if so, store the declaration name in the context
+            {-| if so, store the declaration name in the context -}
                 Rule.OnEnter -> 
                     ([], Node.value n.name :: context)
                 Rule.OnExit -> 
